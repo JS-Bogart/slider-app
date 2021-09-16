0bffca3aa35664ce4a9da4ae03a1538feacba713
Bogey <jeffreysbogart@gmail.com>
2021-09-15 18:07:40 -0700

    Add slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react";
+
+const Slider = () => {
+
+  const [sliderData, setSliderData] = useState(null);
+
+  return(
+    <div>
+      <header>
+        Slider App
+      </header>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Slider;
